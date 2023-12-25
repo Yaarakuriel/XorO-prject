261ea201d2e62890f5cb0d6cd2d8229e37283ed3
yaarakuriel <yaarakuriel@gmail.com>
2023-12-25 19:12:09 +0200

    added user choice,comp choice, win, win note funcs

--- game.py
+++ game.py
@@ -1,6 +1,12 @@
+import random
+
 class Game():
     def __init__(self):
         self.vector= [1,2,3,4,5,6,7,8,9]
+        self.board = ['','','','','','','','','']
+        self.win= False
+        #player, True-user, False-computer
+        self.player = True
         
     def choice(self,value):
         n = 1
@@ -18,4 +24,39 @@ class Game():
                 c = 0
             else:
                 value = input('this position is taken. please pick a different number\n')
+    
+    def user_req(self):
+        value = input('please, type the number  \n')
+        
+    
+    def comp_req(self):
+        value = random.choice(self.vector)
+        
+    def win(self):
+        #rows
+        if self.board[1] == self.board[2] == self.board[3]:
+            self.win = True
+        elif self.board[4] == self.board[5] == self.board[6]:
+            self.win = True
+        elif self.board[7] == self.board[8] == self.board[9]:
+            self.win = True
+        #cross
+        elif self.board[1] == self.board[5] == self.board[9]:
+            self.win = True
+        elif self.board[3] == self.board[5] == self.board[7]:
+            self.win = True
+        #columns
+        elif self.board[1] == self.board[4] == self.board[7]:
+            self.win = True
+        elif self.board[2] == self.board[5] == self.board[8]:
+            self.win = True
+        elif self.board[3] == self.board[6] == self.board[9]:
+            self.win = True
+            
+    def wining_note(self):
+        if self.player == True:
+            print('Congratulations! You won the game. see you on the next game.')  
+        else:
+            print('You lost! maybe next time : - )')    
+        
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-
+from game import Game
 opening = "Welcome To X Mix Drix Game! \nfrom now on, You'll play X . for you to play, enter the number you'd like X to be instead\n"
 vector = [1,2,3,4,5,6,7,8,9]
 board = f"""  {vector[0]}  |  {vector[1]}  |  {vector[2]}  \n\n________________\n\n  {vector[3]}  |  {vector[4]}  |  {vector[5]}  \n\n________________\n\n  {vector[6]}  |  {vector[7]}  |  {vector[8]}  \n"""
 print(opening)
 print(board)
-value = input('please, type the number  \n')
+
 vector = [ '','', '','' ,'' ,'' ,'' ,'' ,'']
 
+XorO= Game()
+XorO.user_req()
     
