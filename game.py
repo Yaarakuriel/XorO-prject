fecbbb5de4372d6f0336ea09141a6c412e80c606
yaarakuriel <yaarakuriel@gmail.com>
2023-12-26 21:21:22 +0200

    fixing value probs when entering a num that's already taken

--- game.py
+++ game.py
@@ -7,46 +7,48 @@ class Game():
         self.win= False
         #player, True-user, False-computer
         self.player = True
+        self.value = 0
     
     def show_board(self):
         b = f"""  {self.board[0]}  |  {self.board[1]}  |  {self.board[2]}  \n\n________________\n\n  {self.board[3]}  |  {self.board[4]}  |  {self.board[5]}  \n\n________________\n\n  {self.board[6]}  |  {self.board[7]}  |  {self.board[8]}  \n"""
         print(b)
         
-    def choice(self,value):
+    def choice(self):
         n = 1
         while n == 1:
             try:
-                nval= int(value)
-                n = 0
+                nval= int(self.value)
             except(ValueError):
                 print('valueerror')
-                value= input('please, provide a number from 1 to 8\n')
+                self.value= input('please, provide a number from 1 to 9\n')
                 n = 1
             if nval < 1 or nval > 9:
                 print('numbering')
                 n = 1
-                value= input('please, provide a number from 1 to 8\n')
-            elif nval in self.vector:
+                self.value= input('please, provide a number from 1 to 9\n')
+            if nval in self.vector:
                 self.vector.remove(nval)
                 n = 0
             else:
-                value = input('this position is taken. please pick a different number\n')
+                self.value = input('this position is taken. please pick a different number\n')
                 n = 1
     
     def user_req(self):
         value = input('please, type the number  \n')
-        self.choice(value)
+        self.value = value
+        self.choice()
         print(self.vector)
-        self.board[int(value)-1]='X'
+        print(f'the value is{self.value}')
+        self.board[int(self.value)-1]='X'
         self.player = True
         self.show_board()
         #self.win()
         
     
     def comp_req(self):
-        value = random.choice(self.vector)
-        self.choice(value)
-        self.board[int(value)-1]='O'
+        self.value = random.choice(self.vector)
+        self.choice()
+        self.board[int(self.value)-1]='O'
         self.player = False
         print('the comp chose:')
         self.show_board()
