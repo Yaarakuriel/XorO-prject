fb5e26eecc92e78641f98a0f5f73dae924cd7c9f
yaarakuriel <yaarakuriel@gmail.com>
2023-12-26 18:38:43 +0200

    show board addition+ trigger function in user req

--- game.py
+++ game.py
@@ -7,6 +7,9 @@ class Game():
         self.win= False
         #player, True-user, False-computer
         self.player = True
+    
+    def show_board(self):
+        b = f"""  {self.board[0]}  |  {self.board[1]}  |  {self.board[2]}  \n\n________________\n\n  {self.board[3]}  |  {self.board[4]}  |  {self.board[5]}  \n\n________________\n\n  {self.board[6]}  |  {self.board[7]}  |  {self.board[8]}  \n"""
         
     def choice(self,value):
         n = 1
@@ -30,31 +33,44 @@ class Game():
     
     def user_req(self):
         value = input('please, type the number  \n')
+        value = random.choice(self.vector)
+        self.choice(value)
+        self.board[value-1]='X'
+        self.player = True
+        self.show_board()
+        self.win()
         
     
     def comp_req(self):
         value = random.choice(self.vector)
+        self.choice(value)
+        self.board[value-1]='O'
+        self.player = False
+        self.show_board()
+        self.win()
+        
         
     def win(self):
         #rows
         if self.board[1] == self.board[2] == self.board[3]:
-            self.win = True
+            self.wining_note()
         elif self.board[4] == self.board[5] == self.board[6]:
-            self.win = True
+            self.wining_note()
         elif self.board[7] == self.board[8] == self.board[9]:
-            self.win = True
+            self.wining_note()
         #cross
         elif self.board[1] == self.board[5] == self.board[9]:
-            self.win = True
+            self.wining_note()
         elif self.board[3] == self.board[5] == self.board[7]:
-            self.win = True
+            self.wining_note
         #columns
         elif self.board[1] == self.board[4] == self.board[7]:
-            self.win = True
+            self.wining_note()
         elif self.board[2] == self.board[5] == self.board[8]:
-            self.win = True
+            self.wining_note()
         elif self.board[3] == self.board[6] == self.board[9]:
-            self.win = True
+            self.wining_note()
+            
     def wining_note(self):
         if self.player == True:
             print('Congratulations! You won the game. see you on the next game.')  
